a89ef676b57615a8d14744979240aa4cfd84203a
Ks2341 <yobeb96@gmail.com>
2024-05-16 21:32:06 +0200

    5

--- src/App.vue
+++ src/App.vue
@@ -112,9 +112,9 @@ export default {
           "callsign": "Zippo",
           "alias": "Ricardo Suarez",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Necrophile"
+          "corpro": "IPS-N",
+          "frame": "Drake",
+          "mech": "The Anxiety Machine"
         },
         {
           "callsign": "Starboard",
